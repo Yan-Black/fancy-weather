f0d490ee731b70c7693b0fd08f479dacd45da540
Yan Andryshkin <yan.7feb@gmail.com>
2020-06-04 16:09:36 +0300

    fix: fix forecast days names

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -13,35 +13,35 @@ function ForeacstBlock(props) {
     return (
         <div className="forecast">
             <div className="forecast-info">
-                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay()]}</h5>
+                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 1]}</h5>
                 <div className="forecast-temp">
                     <img className="forecast-icon" src={weatherConditionsIcons[props.src[0]]} alt=""/>
                     <p className="temp-val">{temp[0].toFixed(0) + '°'}</p>
                 </div>
             </div>
             <div className="forecast-info">
-                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 1]}</h5>
+                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 2]}</h5>
                 <div className="forecast-temp">
                     <img className="forecast-icon" src={weatherConditionsIcons[props.src[1]]} alt=""/>
                     <p className="temp-val">{temp[1].toFixed(0) + '°'}</p>
                 </div>
             </div>
             <div className="forecast-info">
-                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 2]}</h5>
+                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 3]}</h5>
                 <div className="forecast-temp">
                     <img className="forecast-icon" src={weatherConditionsIcons[props.src[2]]} alt=""/>
                     <p className="temp-val">{temp[2].toFixed(0) + '°'}</p>
                 </div>
             </div>
             <div className="forecast-info">
-                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 3]}</h5>
+                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 4]}</h5>
                 <div className="forecast-temp">
                     <img className="forecast-icon" src={weatherConditionsIcons[props.src[3]]} alt=""/>
                     <p className="temp-val">{temp[3].toFixed(0) + '°'}</p>
                 </div>
             </div>
             <div className="forecast-info">
-                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 4]}</h5>
+                <h5 data-forecast="translate" className="day">{chooseDaysMonthArray()[new Date().getDay() + 5]}</h5>
                 <div className="forecast-temp">
                     <img className="forecast-icon" src={weatherConditionsIcons[props.src[4]]} alt=""/>
                     <p className="temp-val">{temp[4].toFixed(0) + '°'}</p>
